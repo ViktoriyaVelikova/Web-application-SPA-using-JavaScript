e70704e78e9c994cead6223bef6106589bb210ed
Viktoriya Velikova <velikovaviktoriya@yahoo.com>
2022-03-25 18:06:22 +0200

    Added router and views

--- Music-App-Single-Page-Application/app.js
+++ Music-App-Single-Page-Application/app.js
@@ -2,5 +2,44 @@
 
  import layout from './views/layout.js';
  import home from './views/home.js';
+ import login from './views/login.js';
+ import register from './views/register.js';
+ import create from './views/create.js';
+ import edit from './views/edit.js';
+ import details from './views/details.js';
+ import search from './views/search.js';
+ import catalog from './views/catalog.js';
 
- render(layout(home()), document.getElementById('box'));
+ const routes = [{
+     path: '/',
+     template: home
+ }, {
+     path: '/login',
+     template: login
+ }, {
+     path: '/register',
+     template: register
+ }, {
+     path: '/create',
+     template: create
+ }, {
+     path: '/edit',
+     template: edit
+ }, {
+     path: '/details',
+     template: details
+ }, {
+     path: '/search',
+     template: search
+ }, {
+     path: '/catalog',
+     template: catalog
+ }];
+
+ const router = (path) => {
+     let route = routes.find(x => x.path == path);
+
+     render(layout(route.template()), document.getElementById('box'));
+ }
+
+ router(location.pathname);
--- Music-App-Single-Page-Application/views/header.js
+++ Music-App-Single-Page-Application/views/header.js
@@ -4,16 +4,16 @@ export default () => html `
 <header>
 <nav>
    <img src="./images/headphones.png">
-   <a href="#">Home</a>
+   <a href="/">Home</a>
    <ul>
        <!--All user-->
-       <li><a href="#">Catalog</a></li>
-       <li><a href="#">Search</a></li>
+       <li><a href="/catalog">Catalog</a></li>
+       <li><a href="/search">Search</a></li>
        <!--Only guest-->
-       <li><a href="#">Login</a></li>
-       <li><a href="#">Register</a></li>
+       <li><a href="/login">Login</a></li>
+       <li><a href="/register">Register</a></li>
        <!--Only user-->
-       <li><a href="#">Create Album</a></li>
+       <li><a href="/create">Create Album</a></li>
        <li><a href="#">Logout</a></li>
    </ul>
 </nav>
